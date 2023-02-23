3d94d6d8ed395281dee41edc122ad852077d3429
Duc Phan <ptmdmusic@gmail.com>
2023-02-23 12:10:39 -0800

    add libraries, setup tailwind and eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import "./globals.scss";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      Hello World. <Link href="/about">About</Link>
+      Hello World.{" "}
+      <Link href="/about" className="text-skin-base">
+        About
+      </Link>
     </div>
   );
 }
