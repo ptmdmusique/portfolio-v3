175309a261e9eaf56f50a9cebda3ed2f92f406e9
Duc Phan <ptmdmusic@gmail.com>
2023-02-24 13:46:13 -0800

    fix icon hydration issue

--- src/loaders/icon.ts
+++ src/loaders/icon.ts
@@ -1,33 +1,34 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
 import * as fab from "@fortawesome/free-brands-svg-icons";
 import * as fas from "@fortawesome/pro-solid-svg-icons";
 // import * as fal from "@fortawesome/pro-light-svg-icons";
 import * as far from "@fortawesome/pro-regular-svg-icons";
 // import * as fad from "@fortawesome/pro-duotone-svg-icons";
 
+// https://github.com/FortAwesome/Font-Awesome/issues/19348
+const { library } = require("@fortawesome/fontawesome-svg-core");
+
 export const initializeIconList = () => {
-  const iconList = [
-    far.faEllipsisV,
+  library.add(
+    ...[
+      far.faEllipsisV,
 
-    far.faSun,
-    far.faMoon,
+      far.faSun,
+      far.faMoon,
 
-    fas.faCogs,
+      fas.faCogs,
 
-    fab.faGithub,
-    fab.faLinkedin,
+      fab.faGithub,
+      fab.faLinkedin,
 
-    // * Route info
-    far.faHeart,
-    far.faAddressCard,
-    far.faLightbulbOn,
-    far.faEnvelope,
-    fas.faHeart,
-    fas.faAddressCard,
-    fas.faLightbulbOn,
-    fas.faEnvelope,
-  ];
-
-  // Set up icon libraries
-  library.add(...iconList);
+      // * Route info
+      far.faHeart,
+      far.faAddressCard,
+      far.faLightbulbOn,
+      far.faEnvelope,
+      fas.faHeart,
+      fas.faAddressCard,
+      fas.faLightbulbOn,
+      fas.faEnvelope,
+    ],
+  );
 };
