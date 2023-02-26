843055a56bceb6557d467aed7ab9d867928c61bc
Duc Phan <ptmdmusic@gmail.com>
2023-02-26 12:01:26 -0800

    replace navbar active border with slider indicator

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,6 +9,7 @@ import { useRouter } from "next/router";
 import { AppLogo } from "../AppLogo/AppLogo";
 import { useMedia } from "react-use";
 import { AppSettingContent } from "../AppSettingContent/AppSettingContent";
+import { createRef, forwardRef, useEffect, useRef, useState } from "react";
 
 const breakpoints = {
   sm: 640,
@@ -25,35 +26,54 @@ interface NavItemProps {
   routePath: typeof routePathList[number];
   isActive: boolean;
 }
-const NavItem = ({
-  iconName,
-  isActive,
-  itemKey,
-  label,
-  routePath,
-}: NavItemProps) => {
-  const { t } = useTranslation();
+const NavItem = forwardRef<HTMLLIElement, NavItemProps>(
+  ({ iconName, isActive, itemKey, label, routePath }, ref) => {
+    const { t } = useTranslation();
 
-  return (
-    <li
-      className={cx("nav-item", `nav-item__${itemKey}`, {
-        "nav-item--active": isActive,
-      })}
-    >
-      <Link href={routePath} className="nav-item__link">
-        <Icon icon={[isActive ? "fas" : "far", iconName]} />
-        <p>{t(label)}</p>
-      </Link>
-    </li>
-  );
-};
+    return (
+      <li
+        ref={ref}
+        className={cx("nav-item", `nav-item__${itemKey}`, {
+          "nav-item--active": isActive,
+        })}
+      >
+        <Link href={routePath} className="nav-item__link">
+          <Icon icon={[isActive ? "fas" : "far", iconName]} />
+          <p>{t(label)}</p>
+        </Link>
+      </li>
+    );
+  },
+);
+NavItem.displayName = "NavItem";
 
-const MobileNavContainer = () => {
+const RoutePathList = ({ isLg }: { isLg: boolean }) => {
   const router = useRouter();
+  const listItemRefList = useRef(
+    Array.from({ length: routePathList.length }, () =>
+      createRef<HTMLLIElement>(),
+    ),
+  );
+
+  const [position, setPosition] = useState(0);
+
+  useEffect(() => {
+    // If isLg, use top position of nav item, else use left
+    const activeItem =
+      listItemRefList.current[routePathList.indexOf(router.pathname)]?.current;
+
+    if (activeItem) {
+      const position = isLg
+        ? activeItem?.offsetTop ?? 0
+        : activeItem?.offsetLeft ?? 0;
+
+      setPosition(position);
+    }
+  }, [router.pathname, isLg]);
 
   return (
-    <ul className="nav-container nav-container__mobile">
-      {routePathList.map((routePath) => {
+    <>
+      {routePathList.map((routePath, index) => {
         const { iconName, key, label } = routeInfoMap[routePath];
         const isActive = router.pathname === routePath;
 
@@ -65,15 +85,26 @@ const MobileNavContainer = () => {
             label={label}
             routePath={routePath}
             isActive={isActive}
+            ref={listItemRefList.current[index]}
           />
         );
       })}
-    </ul>
+
+      <div
+        className="nav-indicator"
+        style={isLg ? { right: 0, top: position } : { left: position, top: 0 }}
+      />
+    </>
   );
 };
 
+const MobileNavContainer = () => (
+  <ul className="nav-container nav-container__mobile">
+    <RoutePathList isLg={false} />
+  </ul>
+);
+
 const DesktopNavContainer = () => {
-  const router = useRouter();
   const { t } = useTranslation();
 
   return (
@@ -83,21 +114,7 @@ const DesktopNavContainer = () => {
       </div>
 
       <ul className="route-list-container">
-        {routePathList.map((routePath) => {
-          const { iconName, key, label } = routeInfoMap[routePath];
-          const isActive = router.pathname === routePath;
-
-          return (
-            <NavItem
-              key={key}
-              itemKey={key}
-              iconName={iconName}
-              label={label}
-              routePath={routePath}
-              isActive={isActive}
-            />
-          );
-        })}
+        <RoutePathList isLg />
       </ul>
 
       <Popover
