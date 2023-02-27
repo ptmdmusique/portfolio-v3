ef0249b070560192900486f20edb6eb45f20fdcd
Duc Phan <ptmdmusic@gmail.com>
2023-02-26 20:54:17 -0800

    add og image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -30,6 +30,31 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
       <Head>
         <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+
+        <meta property="og:image" content="https://i.imgur.com/ipcYMd2.png" />
       </Head>
 
       <ThemeContext.Provider value={{ theme, setTheme }}>
